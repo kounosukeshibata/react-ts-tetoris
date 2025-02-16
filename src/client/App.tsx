a8723f001be21d7fabcbd55398566944ce6aa8ae
shibata_konosuke <shibata_k@r.recruit.co.jp>
2025-02-16 19:20:56 +0900

    環境構築を完了

---
+++ src/client/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import { useState } from "react";
+
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
