f623299a570311802efa2925f6de67da4ff80f55
shibata_konosuke <shibata_k@r.recruit.co.jp>
2025-02-21 18:37:18 +0900

    画像の差し替えやその他不要なファイルを削除

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,10 +1,7 @@
 import "./App.css";
 import Container from "./components/Container";
-import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Container />
